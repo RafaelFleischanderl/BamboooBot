957f5ec956ffba5daf115fbedaff33150512401a
RafaelFleischanderl <rafael.w.fleischanderl@gmail.com>
2024-11-29 17:32:30 +0100

    testing on real printer - works!

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from printer_connection import Bambu
 from discord.ext import commands
 
 load_dotenv()
-# bambu = Bambu()
+bambu = Bambu()
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents) # prefix is deprecated - only slash commands are supported by discord
@@ -18,10 +18,16 @@ async def on_ready():
         print(f"Synced {len(synced)} commands")
     except Exception as e:
         print(f"Error syncing commands: {e}")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Ready to serve!"))
 
 @bot.tree.command(name="hello", description="Say hello!")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hello, {interaction.user}!")
+    await interaction.response.defer()  # Defer the response to avoid timeout
+    try:
+        status = await bambu.get_status()
+        await interaction.followup.send(f"Hello, the printer is currently{"" if status.online else " not" } printing!")
+    except Exception as _:
+        await interaction.followup.send(content="Sorry, i crashed :(")
 
 bot.run(os.getenv('BOT_TOKEN'))
-# TODO: set bot private
+
--- printer_connection.py
+++ printer_connection.py
@@ -1,5 +1,7 @@
 import os
-from bambu_connect import BambuClient
+from bambu_connect import BambuClient, PrinterStatus
+import time
+import asyncio
 
 
 class Bambu:
@@ -8,7 +10,34 @@ class Bambu:
         self.__access_code = os.getenv('PRINTER_ACCESS_CODE')
         self.__serial_number = os.getenv('PRINTER_SERIAL_NUMBER')
 
-        self.__BambuClient = BambuClient(self.__hostname, self.__access_code, self.__serial_number)
+        self.__bambu_client = BambuClient(self.__hostname, self.__access_code, self.__serial_number)
 
-    def get_status(self):
-        return self.__BambuClient.get_status() # unfinished
+        self.__bambu_client.start_watch_client(self.__on_watch_client_trigger,self.__on_watch_client_connect)
+        self.__capture_next_status = True
+        self.__latest_status = None
+        self.__callback_count = 0
+
+    def __on_watch_client_trigger(self, msg : PrinterStatus):
+        if self.__capture_next_status:
+            self.__capture_next_status = False
+            self.__latest_status = msg
+        self.__callback_count += 1
+        print(f"Callback count: {self.__callback_count}")
+
+    def __on_watch_client_connect(self):
+        print("Watch client connected to printer")
+
+    async def get_status(self):
+
+        self.__capture_next_status = True
+        while self.__latest_status is None:
+            await asyncio.sleep(0.1)
+        copy = self.__latest_status
+        self.__latest_status = None
+        self.__capture_next_status = False
+        # self.__bambu_client.stop_watch_client()
+        print(copy)
+        return copy
+
+    def get_camera_frame(self):
+        return self.__bambu_client.capture_camera_frame()
