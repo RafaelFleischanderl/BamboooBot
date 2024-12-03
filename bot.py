398cb1da135bc474c3dc2139329ca1bcb51914af
RafaelFleischanderl <rafael.w.fleischanderl@gmail.com>
2024-12-03 14:51:19 +0100

    changes and a python bug i don't understand

--- bot.py
+++ bot.py
@@ -3,12 +3,21 @@ from dotenv import load_dotenv
 import discord
 from printer_connection import Bambu
 from discord.ext import commands
+from io import BytesIO
 
 load_dotenv()
 bambu = Bambu()
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents) # prefix is deprecated - only slash commands are supported by discord
+
+__CONNECTION_FAILED_RESPONSE = "Failed to connect to printer!"
+def ensure_connection():
+    if bambu.is_connected():
+        return True
+    if bambu.try_reconnect():
+        return True
+    return False
 
 @bot.event
 async def on_ready():
@@ -18,15 +27,39 @@ async def on_ready():
         print(f"Synced {len(synced)} commands")
     except Exception as e:
         print(f"Error syncing commands: {e}")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Ready to serve!"))
+    if ensure_connection():
+        print("Connected to bambu-printer")
+    else:
+        print("Failed to connect to bambu-printer")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Ready to observe!"))
 
-@bot.tree.command(name="hello", description="Say hello!")
-async def hello(interaction: discord.Interaction):
+@bot.tree.command(name="status", description="Get the current printer status!")
+async def status(interaction: discord.Interaction):
+    if not ensure_connection():
+        await interaction.response.send_message(__CONNECTION_FAILED_RESPONSE)
+        return
+
     await interaction.response.defer()  # Defer the response to avoid timeout
     try:
         status = await bambu.get_status()
-        await interaction.followup.send(f"Hello, the printer is currently{"" if status.online else " not" } printing!")
+        await interaction.followup.send(f"The printer is currently{"" if status.online else " not" } printing!")
     except Exception as _:
+        await interaction.followup.send(content="Sorry, i crashed :(")
+
+@bot.tree.command(name="cam", description="Get a realtime screenshot of the current print!")
+async def cam(interaction: discord.Interaction):
+    if not ensure_connection():
+        await interaction.response.send_message(__CONNECTION_FAILED_RESPONSE)
+        return
+
+    await interaction.response.defer()  # Defer the response to avoid timeout
+    try:
+        frame = bambu.get_camera_frame()
+        image_bytes = BytesIO(frame)
+        file = discord.File(image_bytes, filename="frame.png")
+        await interaction.followup.send(file=file)
+    except Exception as e:
+        print(e)
         await interaction.followup.send(content="Sorry, i crashed :(")
 
 bot.run(os.getenv('BOT_TOKEN'))
--- printer_connection.py
+++ printer_connection.py
@@ -5,24 +5,34 @@ import asyncio
 
 
 class Bambu:
+
     def __init__(self):
         self.__hostname = os.getenv('PRINTER_IP')
         self.__access_code = os.getenv('PRINTER_ACCESS_CODE')
         self.__serial_number = os.getenv('PRINTER_SERIAL_NUMBER')
+        self.__is_connected_to_printer = False
 
-        self.__bambu_client = BambuClient(self.__hostname, self.__access_code, self.__serial_number)
+    def try_reconnect(self):
+        try:
+            self.__bambu_client = BambuClient(self.__hostname, self.__access_code, self.__serial_number)
+            self.__is_connected_to_printer = True
+        except Exception as e:
+            print(f"Failed to connect to bambu printer")
+            self.__is_connected_to_printer = False
+        return self.__is_connected_to_printer
 
+    def is_connected(self):
+        return self.__is_connected_to_printer
+
+    def start_watch_client(self):
         self.__bambu_client.start_watch_client(self.__on_watch_client_trigger,self.__on_watch_client_connect)
         self.__capture_next_status = True
         self.__latest_status = None
-        self.__callback_count = 0
 
     def __on_watch_client_trigger(self, msg : PrinterStatus):
         if self.__capture_next_status:
             self.__capture_next_status = False
             self.__latest_status = msg
-        self.__callback_count += 1
-        print(f"Callback count: {self.__callback_count}")
 
     def __on_watch_client_connect(self):
         print("Watch client connected to printer")
@@ -35,7 +45,6 @@ class Bambu:
         copy = self.__latest_status
         self.__latest_status = None
         self.__capture_next_status = False
-        # self.__bambu_client.stop_watch_client()
         print(copy)
         return copy
 
